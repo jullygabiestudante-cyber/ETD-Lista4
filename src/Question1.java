dde6e91ce6e36a0ab36997148c4211911740ec20
Julia-Gabriele <jullygaiestudante@gmail.com>
2026-02-28 19:19:45 -0300

    Atualizando files

--- src/Question1.java
+++ src/Question1.java
@@ -1,2 +1,60 @@
 public class Question1 {
+
+    public static void main(String[] args) {
+
+        double [] notas = {7.5,4.0,9.3,5.5,8.0,6.5,3.2,10.0,5.8};
+
+        double soma = soma(notas);
+        double media = media (soma,notas.length);
+        double acimaMedia = maiorMedia(notas, media);
+        double maior = maiorNumero(notas);
+
+        System.out.println ("A soma e:" + soma);
+        System.out.println("A media das notas: " + media);
+        System.out.println ("A quantidade de numeros acima da media e: " + acimaMedia);
+        System.out.println ("O maior numero e :" + maior) ;
+
+
+    }
+    public  static double soma(double [] notas){
+        double soma =0;
+        int i;
+        for (i =0 ; i < notas.length; i++){
+            soma = notas[i] + soma;
+        }
+        return soma;
+
+    }
+    public static double media ( double soma,  int quantidade ){
+        double media =0;
+        return media = soma/quantidade;
+    }
+
+    public static int maiorMedia ( double [] notas, double media ){
+        int contador = 0, i;
+        for (i =0 ; i < notas.length; i++) {
+            if (notas[i] > media) {
+                contador += 1;
+            }
+        }
+        return contador;
+    }
+
+    public static double maiorNumero(double [] notas){
+        double maior = 0;
+        int i;
+        for (i =0 ; i < notas.length; i++){
+            double auxiliar = notas[i];
+            if (auxiliar > maior){
+                maior = auxiliar;
+            }
+        }
+        return maior;
+    }
+
+
+
+
+
 }
+
--- src/Question2.java
+++ src/Question2.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class Question2 {
+
+
+    public static void main(String[] args) {
+        int numero = Leitura();
+
+        int notasDe50 = Calcularnotas(numero, 50);
+        int resto50 = numero % 50;
+        int notasDe20 = Calcularnotas(resto50,20);
+        int resto20= resto50 % 20;
+        int notasDe10 =Calcularnotas(resto20,10);
+
+        System.out.println("Sao " + notasDe50 + " notas de 50");
+        System.out.println("Sao " + notasDe20 + " notas de 20");
+        System.out.println("Sao " + notasDe10 + " notas de 10");
+
+    }
+        public static int Leitura (){
+
+            // numero lido
+            int n = 0;
+            System.out.println("Digite um numero:");
+            Scanner scanner = new Scanner(System.in);
+            return  scanner.nextInt();
+        }
+
+
+    public static int Calcularnotas (int  valor, int valorNotas){
+        return valor / valorNotas;
+    }
+
+
+
 }
