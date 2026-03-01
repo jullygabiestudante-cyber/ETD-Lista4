a9c84b04ff742c662543ab9ec7edc18446bcef1c
Julia-Gabriele <jullygaiestudante@gmail.com>
2026-02-28 22:28:29 -0300

    Adicionando Files

--- src/Question5.java
+++ src/Question5.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
 public class Question5 {
+    public static void main (String[]args) {
+        int[] numeros = new int[5];
+        leitura(numeros);
+        boolean validacaoRepetidos = verificacao(numeros);
+
+        if (validacaoRepetidos == true ) {
+            System.out.println("Tem números repetidos.");
+        } else {
+            System.out.println("Não tem números repetidos.");
+        }
+
+}
+
+    public static  void leitura (int [] numeros){
+        int i, j;
+        int cont =1;
+        Scanner scanner = new Scanner(System.in);
+        for (i = 0; i < numeros.length; i++) {
+            System.out.print("Digite o " + cont + " número : ");
+            cont++;
+            numeros[i]= scanner.nextInt();
+        }
+
+    }
+    public static boolean verificacao(int [] numeros){
+        int i, j;
+        boolean repetido  = false;
+        for (i = 0; i < numeros.length - 1; i++) {
+
+            for (j = i + 1; j < numeros.length; j++) {
+                if (numeros[i] == numeros[j]) {
+                    repetido = true;
+                }
+            }
+
+        }
+        return  repetido;
+
+    }
+
+
+
+
 }
--- src/Question6.java
+++ src/Question6.java
@@ -1,2 +1,30 @@
 public class Question6 {
+
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+        System.out.println("Números na Posição Atual :");
+        imprimirVetor(numeros);
+        System.out.println("Números na Posição Invertida :");
+        inverter_list(numeros);
+        imprimirVetor(numeros);
+    }
+
+
+    public static void inverter_list(int[] numeros) {
+        int i, j;
+        for (i = 0, j = numeros.length - 1; i < j; i++, j--) {
+            int auxiliar = numeros[i];
+            numeros[i] = numeros[j];
+            numeros[j] = auxiliar;
+
+        }
+    }
+
+    public static void imprimirVetor(int[] numeros) {
+        int i;
+        for (i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
 }
